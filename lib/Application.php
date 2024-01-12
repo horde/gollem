047246d84b62e683a80e6be754007cb6fa9988d0
Ralf Lang <ralf.lang@ralf-lang.de>
2024-01-12 11:01:08 +0100

    Mark as a H6 app.

--- lib/Application.php
+++ lib/Application.php
@@ -50,7 +50,7 @@ class Gollem_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (5.0.0alpha2)';
+    public $version = 'H6 (5.0.0alpha2)';
 
     /**
      * Server key used in logged out session.
