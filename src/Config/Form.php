fbf7313732f95d6892bad3431f163340793d2b60
Ralf Lang <ralf.lang@ralf-lang.de>
2025-06-02 19:21:56 +0200

    Add Field but fall back to xml form

--- src/Config/Form.php
+++ src/Config/Form.php
@@ -2,7 +2,17 @@
 namespace Horde\Gollem\Config;
 use Horde_Form;
 use Horde_Config_Form;
-
+use Horde;
 class Form extends Horde_Config_Form
-{
+{    
+    public function __construct($vars, $app = 'gollem')
+    {
+        $this->addVariable(_('Backend Administration'), 'add_backend', 'link', true, false, _('Add a gollem files backend'), [[
+            'url' => Horde::url($GLOBALS['registry']->get('webroot', $app) . '/admin/backend/new', true),
+            'text' => _('Add Backend'),
+            'title' => 'plus'
+        ]]
+        );
+        parent::__construct($vars, $app);
+    }
 }
