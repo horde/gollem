f02e715d30b8d5fb90c861e7ee73135ad1fd14fb
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-21 08:20:42 +0200

    chore(release): bump version to 5.0.0-beta2
    Release version 5.0.0-beta2
    
    test: Initialize a minimal test suite for gollem
    docs: Convert documents to markdown
    style: php-cs-fixer
    chore: Add empty routes file
    docs: create/update conf.php.dist
    doc: Mention 'mlsd' parameter in backends.php
    fix: Support UNC paths
    refactor: Remove most reliance on prototype.js
    fix: More defensive code when trying to drop tables
    docs: Clarify where to put local overrides

--- lib/Application.php
+++ lib/Application.php
@@ -59,7 +59,7 @@ class Gollem_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-beta1';
+    public $version = '5.0.0-beta2';
 
     /**
      * Server key used in logged out session.
