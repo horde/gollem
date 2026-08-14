70c2563fc76b623597140de1d262bb6a50210ce1
Ralf Lang <ralf.lang@ralf-lang.de>
2026-08-14 08:18:02 +0200

    chore(release): bump version to 5.0.0-beta3
    Release version 5.0.0-beta3
    
    chore: ignore local web/ and var/ install dirs
    chore: Update workflow dependency
    refactor(injector): Widen type hints from Horde_Injector to Horde_Injector|Injector
    refactor(strftime): Migrate to native usage of IntlDateFormatter and DateTime

--- lib/Application.php
+++ lib/Application.php
@@ -59,7 +59,7 @@ class Gollem_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-beta2';
+    public $version = '5.0.0-beta3';
 
     /**
      * Server key used in logged out session.
